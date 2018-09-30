b0bb0f44027037b16837430a286f906e090f5822
rahulsai1999 <34063423+rahulsai1999@users.noreply.github.com>
2018-09-30 13:08:21 +0530

    final changes

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var mongoose = require("mongoose");
 var request=require("request");
 var bodyParser=require("body-parser");
 var passport = require("passport");
-var localStrategy = require("passport-local");
+var localStrategy = require("passport-local").Strategy;
 var passportLocalMongoose = require("passport-local-mongoose");
 var methodOverride = require("method-override");
+const flash = require("express-flash-messages");
+app.use(flash());
 var app=express();
 
 mongoose.connect("mongodb://admin:admin83@ds159812.mlab.com:59812/loginegapp");
@@ -19,26 +21,102 @@ app.set("view engine","ejs");
 
 //auth requirements
 app.use(require("express-session")({
-   secret:"picmoraapp",
    resave:false,
-   saveUninitialized:false
+   saveUninitialized:false,
+   secret:"randomsecret"
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+passport.deserializeUser(function(userId, done) {
+    User.findById(userId, (err, user) => done(err, user));
+  });
+
+const local = new LocalStrategy((username, password, done) => {
+    User.findOne({ username })
+      .then(user => {
+        if (!user || !user.validPassword(password)) {
+          done(null, false, { message: "Invalid username/password" });
+        } else {
+          done(null, user);
+        }
+      })
+      .catch(e => done(e));
+  });
+
+passport.use("local", local);
 
 app.use(function(req,res,next){
   res.locals.currentUser = req.user;
   next();
 })
 
+//--------------------------------------------------//
+//auth
+app.get("/login",isloggedOut,function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register")
+})
+
+app.post("/register", (req, res, next) => {
+    const { username, password } = req.body;
+    User.create({ username, password })
+      .then(user => {
+        req.login(user, err => {
+          if (err) next(err);
+          else res.redirect("/");
+        });
+      })
+      .catch(err => {
+        if (err.name === "ValidationError") {
+          req.flash("Sorry, that username is already taken.");
+          res.redirect("/register");
+        } else next(err);
+      });
+  });
+
+app.post("/login",passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+// ---------------------------------------------------------//
+
+//auth middleware
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) next();
+    else res.redirect("/login");
+  };
+
+const isloggedOut = (req, res, next) => {
+      if (req.isUnauthenticated()) next();
+      else res.redirect("/");
+  };
+
+//---------------------------------------------------------//  
+
 //routes
 
 app.get("/",function(req,res){
     res.render("index");
+});
+
+app.get("/home",isLoggedIn,function(req,res){
+    res.render("home");
 });
 
 app.get("/about",function(req,res){
@@ -69,55 +147,10 @@ app.get("/elements",function(req,res){
     res.render("elements");
 });
 
-
-//auth
-app.get("/login",function(req,res){
-    res.render("login");
-});
-
-app.get("/signup",function(req,res){
-    res.render("signup")
-})
-
-app.post("/register",function(req,res){
-    User.register(new User(
-    {username:req.body.username}),
-    req.body.password,
-        function(err,user){
-            if(err)
-            {
-                console.log(err);
-                res.render("signup");
-            }
-        passport.authenticate("local")(req,res,function(){
-            res.redirect("/login");
-        });
-    });
-});
-
-app.post("/login",passport.authenticate("local",{
-    failureRedirect:"/login"}),
-    function(req,res){
-        res.redirect("/main");
-    });
-
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
-});  
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
-
-//trash
+//trash route
 app.get("*",function(req,res){
     res.render("error");
 });
-
 
 
 //server
